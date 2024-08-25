d581b1a62e15ddb486f111b81dc3eff5d6c8eb28
yaandrango <yaandrango@espe.edu.ec>
2024-08-25 12:33:58 -0500

    add getters y setters entidad movimiento

--- src/main/java/com/example/banking/financialtransactionsservice/model/Movimiento.java
+++ src/main/java/com/example/banking/financialtransactionsservice/model/Movimiento.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.security.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "movimiento", schema = "public")
@@ -13,7 +14,7 @@ public class Movimiento implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Timestamp fecha;
+    private LocalDateTime fecha;
     private String tipoMovimiento;
     private BigDecimal valor;
     private BigDecimal saldo;
@@ -22,5 +23,54 @@ public class Movimiento implements Serializable {
     @JoinColumn(name = "cuenta_id")
     private Cuenta cuenta;
 
+    // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipoMovimiento() {
+        return tipoMovimiento;
+    }
+
+    public void setTipoMovimiento(String tipoMovimiento) {
+        this.tipoMovimiento = tipoMovimiento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
 }
 

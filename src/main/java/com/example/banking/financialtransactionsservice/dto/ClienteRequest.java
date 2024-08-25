7552f7cdf11e8e2d0a79c346ad8ff7ce67cb6666
yaandrango <yaandrango@espe.edu.ec>
2024-08-25 00:48:48 -0500

    add dto clienterequest

---
+++ src/main/java/com/example/banking/financialtransactionsservice/dto/ClienteRequest.java
@@ -0,0 +1,34 @@
+package com.example.banking.financialtransactionsservice.dto;
+
+public class ClienteRequest {
+
+    private Long personaId;
+    private String contrasena;
+    private Boolean estado;
+
+    // Getters y Setters
+
+    public Long getPersonaId() {
+        return personaId;
+    }
+
+    public void setPersonaId(Long personaId) {
+        this.personaId = personaId;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}

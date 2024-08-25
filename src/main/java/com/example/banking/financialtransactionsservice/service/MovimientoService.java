c6fdcac71dd9c1c1ef717b713bd34ac8e954a274
yaandrango <yaandrango@espe.edu.ec>
2024-08-25 12:37:24 -0500

    add servicio MovimientoService

---
+++ src/main/java/com/example/banking/financialtransactionsservice/service/MovimientoService.java
@@ -0,0 +1,31 @@
+package com.example.banking.financialtransactionsservice.service;
+
+import com.example.banking.financialtransactionsservice.model.Movimiento;
+import com.example.banking.financialtransactionsservice.repository.MovimientoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovimientoService {
+    @Autowired
+    private MovimientoRepository movimientoRepository;
+
+    public List<Movimiento> findAll() {
+        return movimientoRepository.findAll();
+    }
+
+    public Optional<Movimiento> findById(Long id) {
+        return movimientoRepository.findById(id);
+    }
+
+    public Movimiento save(Movimiento movimiento) {
+        return movimientoRepository.save(movimiento);
+    }
+
+    public void deleteById(Long id) {
+        movimientoRepository.deleteById(id);
+    }
+}
